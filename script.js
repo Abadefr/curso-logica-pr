0511d716cf4460ccef01d2d02c303f09dcb3353b
franklinux <universomacroexpande@gmail.com>
2024-09-22 13:46:44 -0300

    funçao simples de soma atualizada

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ let x = 9
 let y = 18
 let soma = null
 function somar(x, y){
-    let soma = x + y
+       soma = x + y
     console.log(soma)
 }
 
 somar(1000, 2000)
+document.write(`soma: ${soma}`)
